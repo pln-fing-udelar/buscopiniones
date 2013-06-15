28b5971187643d99b9bfcf72ae52b6ac88e49231
jbonanat@gmail.com <jbonanat@gmail.com@b8bfc974-1341-cdac-c209-07045c9b666c>
2013-06-15 21:43:38 +0000

    meb

--- trunk/Scraper/src/scraper/Main.java
+++ trunk/Scraper/src/scraper/Main.java
@@ -25,37 +25,40 @@ public class Main {
 //			TaggerOpiniones tagger = new TaggerOpiniones("C:\\Fing\\ProyGrado\\opiniones\\", "C:\\Fing\\ProyGrado\\Freeling\\freeling_win\\freeling\\bin\\");
 //			tagger.taggearFreelingDesdeArchivo("C:\\Fing\\ProyGrado\\Freeling\\freeling_win\\freeling\\bin\\prueba.txt", "C:\\Fing\\ProyGrado\\Freeling\\freeling_win\\freeling\\bin\\salida.txt");
 //			return;
-			File folder = new File("C:\\Fing\\ProyGrado\\paginas\\elpais");
-			File[] listOfFiles = folder.listFiles();
-			String nomArchivo = "C:\\Fing\\ProyGrado\\htmlprocesado\\elpais.xml";
+			File folder = new File("C:\\Fing\\ProyGrado\\paginas\\larepublica");
+			File[] listOfFolders = folder.listFiles();
+			String nomArchivo = "C:\\Fing\\ProyGrado\\htmlprocesado\\larepublica.xml";
 			Writer bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(nomArchivo)));
-			for (File file : listOfFiles) {
-				if (file.isFile()) {
+			for (File carpeta_fecha : listOfFolders) {
+				File[] listOfFiles = carpeta_fecha.listFiles();
+				for (File file : listOfFiles) {
+					if (file.isFile()) {
 
-					BASE64Decoder decoder = new BASE64Decoder();
-					byte[] decodedBytes = decoder.decodeBuffer(file.getName());
-					String url = new String(decodedBytes);
-					System.out.println(url);
+						BASE64Decoder decoder = new BASE64Decoder();
+						byte[] decodedBytes = decoder.decodeBuffer(file.getName());
+						String url = new String(decodedBytes);
+						System.out.println(url);
 
-					FileInputStream stream = new FileInputStream(file);
-					FileChannel fc = stream.getChannel();
-					MappedByteBuffer bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
-					String html = Charset.forName("UTF-8").decode(bb).toString();
+						FileInputStream stream = new FileInputStream(file);
+						FileChannel fc = stream.getChannel();
+						MappedByteBuffer bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
+						String html = Charset.forName("UTF-8").decode(bb).toString();
 
-					ProcesadorHTML proc = new ProcesadorHTML(html, url);
-					if (proc.obtenerCharset().equals("iso-8859-1") || proc.obtenerCharset().equals("ISO-8859-1")) {
-						stream = new FileInputStream(file);
-						fc = stream.getChannel();
-						bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
-						html = Charset.forName("ISO-8859-1").decode(bb).toString();
-//						System.out.println(html + "ESTO ES EL FIN");
-						proc = new ProcesadorHTML(html, url);
+						ProcesadorHTML proc = new ProcesadorHTML(html, url);
+						if (proc.obtenerCharset().equals("iso-8859-1") || proc.obtenerCharset().equals("ISO-8859-1")) {
+							stream = new FileInputStream(file);
+							fc = stream.getChannel();
+							bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
+							html = Charset.forName("ISO-8859-1").decode(bb).toString();
+	//						System.out.println(html + "ESTO ES EL FIN");
+							proc = new ProcesadorHTML(html, url);
+						}
+
+
+						String xml = proc.procesar();
+						bw.append(xml);
+						bw.flush();
 					}
-
-
-					String xml = proc.procesar();
-					bw.append(xml);
-					bw.flush();
 				}
 			}
 			bw.close();
