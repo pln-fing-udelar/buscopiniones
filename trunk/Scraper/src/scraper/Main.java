f52204982e2bdf26afa2dc31ecba9a6b8e83205a
jbonanat@gmail.com <jbonanat@gmail.com@b8bfc974-1341-cdac-c209-07045c9b666c>
2013-11-29 20:25:17 +0000

    Linux3

--- trunk/Scraper/src/scraper/Main.java
+++ trunk/Scraper/src/scraper/Main.java
@@ -4,6 +4,14 @@ import java.io.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 import sun.misc.BASE64Decoder;
 
 /**
@@ -77,7 +85,7 @@ public class Main {
 		try {
 			String sistemaOperativo = System.getProperty("os.name");
 			boolean isWin = sistemaOperativo.startsWith("Win");
-
+//			System.out.println((new File("C:\\Fing\\hola")).getName());
 	
 			Configuracion config = new Configuracion(isWin);
 			final int maxIterFreeling = 25;
@@ -121,28 +129,51 @@ public class Main {
 			 * *******************************************************************
 			 */
 //			File status = new File("log.txt");
-			System.out.println("hola che1");
+		
 			File folder = new File(config.getDirTrabajo() + "pruebas");
-			System.out.println("hola che2");
+			
 			File[] listOfMediosPrensa = folder.listFiles();
-			System.out.println("hola che3");
+			
 			Arrays.sort(listOfMediosPrensa);
-			System.out.println("hola che4");
-			File empiezoEnA = new File(config.getDirTrabajo() + "log.txt");
+		
+			
+			File empiezoEnA = new File(config.getDirTrabajo() + "log.xml");
+
+			
 			String empiezoEn = "";
-			if (empiezoEnA.exists()) {
-				empiezoEn = readFile(empiezoEnA, "UTF-8");
+			if(!empiezoEnA.exists()){
+				Writer bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(config.getDirTrabajo() + "log.xml"), "UTF-8"));
+				bw.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n\r\n");
+				bw.append("<log>\r\n");
+				bw.append("<elobservador></elobservador>\r\n");
+				bw.append("<elpais></elpais>\r\n");
+				bw.append("<larepublica></larepublica>\r\n");
+				bw.append("</log>");
+				bw.flush();
+				bw.close();
 			}
+//			if (empiezoEnA.exists()) {
+				DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+				Document doc = dBuilder.parse(empiezoEnA);
+				doc.getDocumentElement().normalize();
+				
+				// para cambiar el valor
+				
+				
+				
+				// empiezoEn = readFile(empiezoEnA, "UTF-8");
+//			}
 			int totMediosPrensa = listOfMediosPrensa.length;	
 			System.out.println(empiezoEn);
-			String[] informacionParaParar = empiezoEn.split(System.getProperty("line.separator"));
+//			String[] informacionParaParar = empiezoEn.split(System.getProperty("line.separator"));
 			File archivoParar;
-			if (empiezoEnA.exists()) {
-				archivoParar = new File(informacionParaParar[0]);
-				System.out.println(informacionParaParar[0]);
-			} else {
-				archivoParar = new File("");
-			}
+//			if (empiezoEnA.exists()) {
+//				archivoParar = new File(informacionParaParar[0]);
+//				System.out.println(informacionParaParar[0]);
+//			} else {
+//				archivoParar = new File("");
+//			}
 
 			DateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
 			//get current date time with Date()
@@ -151,16 +182,17 @@ public class Main {
 
 			int iterMediosPrensa = 0;
 
-			while ((iterMediosPrensa < totMediosPrensa) && (listOfMediosPrensa[iterMediosPrensa].compareTo(archivoParar) <= 0)) {
-				System.out.println(listOfMediosPrensa[iterMediosPrensa].compareTo(archivoParar));
-				iterMediosPrensa++;
-			}
-			System.out.println(iterMediosPrensa);
-			if ((iterMediosPrensa > 0) && (listOfMediosPrensa[iterMediosPrensa - 1].compareTo(archivoParar) < 0)) {
-				iterMediosPrensa--;
-			}
+//			while ((iterMediosPrensa < totMediosPrensa) && (listOfMediosPrensa[iterMediosPrensa].compareTo(archivoParar) <= 0)) {
+//				System.out.println(listOfMediosPrensa[iterMediosPrensa].compareTo(archivoParar));
+//				iterMediosPrensa++;
+//			}
+//			System.out.println(iterMediosPrensa);
+//			if ((iterMediosPrensa > 0) && (listOfMediosPrensa[iterMediosPrensa - 1].compareTo(archivoParar) < 0)) {
+//				iterMediosPrensa--;
+//			}
 			
 			while (iterMediosPrensa < totMediosPrensa) {
+				int contador = 0;
 				File medioPrensa = listOfMediosPrensa[iterMediosPrensa];
 				File[] listOfFolders = medioPrensa.listFiles();
 				Arrays.sort(listOfFolders);
@@ -175,13 +207,14 @@ public class Main {
 				int i = 0;
 				int iterCarpetaFecha = 0;
 				int totCarpetasFecha = listOfFolders.length;
+				archivoParar = new File(doc.getDocumentElement().getElementsByTagName(medioPrensa.getName()).item(0).getTextContent());
 				while ((iterCarpetaFecha < totCarpetasFecha) && (listOfFolders[iterCarpetaFecha].compareTo(archivoParar) <= 0)) {
 					iterCarpetaFecha++;
 				}
 				if ((iterCarpetaFecha > 0) && listOfFolders[iterCarpetaFecha - 1].compareTo(archivoParar) < 0) {
 					iterCarpetaFecha--;
 				}
-				
+				String rutaArch = "";
 				// Para el tema de la semana
 				String xmlNoticia = "";
 				
@@ -198,6 +231,8 @@ public class Main {
 					}
 					while (iterArchivo < totArchivo) {
 						File file = listOfFiles[iterArchivo];
+						rutaArch = file.getPath();
+						contador++;
 						if (file.isFile() && file.length() > 0) {
 							ultimo = file.getName();
 							System.out.println(ultimo);
@@ -240,9 +275,18 @@ public class Main {
 							bwNoticias.flush();
 							xmlNoticia = "";
 							i = 0;
-							Writer status = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(config.getDirTrabajo() + "log.txt"), "UTF-8"));
-							status.write(file.getPath());
-							status.close();
+							
+							//guardo el valor adecuado
+							doc.getDocumentElement().getElementsByTagName(medioActual).item(0).setTextContent(rutaArch);
+							TransformerFactory transformerFactory = TransformerFactory.newInstance();
+							Transformer transformer = transformerFactory.newTransformer();
+							DOMSource source = new DOMSource(doc);
+							StreamResult result = new StreamResult(new File(config.getDirTrabajo() + "log.xml"));
+							transformer.transform(source, result);
+							
+//							Writer status = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(config.getDirTrabajo() + "log.txt"), "UTF-8"));
+//							status.write(file.getPath());
+//							status.close();
 						}
 						iterArchivo++;
 					}
@@ -255,9 +299,18 @@ public class Main {
 						bwNoticias.flush();
 						xmlNoticia = "";
 						i = 0;
-						Writer status = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(config.getDirTrabajo() + "log.txt"), "UTF-8"));
-						status.write(carpeta_fecha.getPath());
-						status.close();
+
+						//guardo el valor adecuado
+						doc.getDocumentElement().getElementsByTagName(medioActual).item(0).setTextContent(rutaArch);
+						TransformerFactory transformerFactory = TransformerFactory.newInstance();
+						Transformer transformer = transformerFactory.newTransformer();
+						DOMSource source = new DOMSource(doc);
+						StreamResult result = new StreamResult(new File(config.getDirTrabajo() + "log.xml"));
+						transformer.transform(source, result);						
+						
+//						Writer status = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(config.getDirTrabajo() + "log.txt"), "UTF-8"));
+//						status.write(carpeta_fecha.getPath());
+//						status.close();
 					}
 					iterCarpetaFecha++;
 				}
@@ -268,16 +321,32 @@ public class Main {
 					bwNoticias.append(xmlNoticia);
 					bwNoticias.flush();
 					xmlNoticia = "";
+					//guardo el valor adecuado	
+					doc.getDocumentElement().getElementsByTagName(medioActual).item(0).setTextContent(rutaArch);
+					TransformerFactory transformerFactory = TransformerFactory.newInstance();
+					Transformer transformer = transformerFactory.newTransformer();
+					DOMSource source = new DOMSource(doc);
+					StreamResult result = new StreamResult(new File(config.getDirTrabajo() + "log.xml"));
+					transformer.transform(source, result);
 				}
-				Writer status = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(config.getDirTrabajo() + "log.txt"), "UTF-8"));
-				status.write(medioPrensa.getPath() + System.getProperty("line.separator"));
-				status.close();
+				
+				//guardo el valor adecuado
+
+				
+//				Writer status = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(config.getDirTrabajo() + "log.txt"), "UTF-8"));
+//				status.write(medioPrensa.getPath() + System.getProperty("line.separator"));
+//				status.close();
+				
 				bw.append("</add>");
 				bwNoticias.append("</add>");
 				bw.flush();
 				bwNoticias.flush();
 				bw.close();
 				bwNoticias.close();
+				if (contador == 0) {
+					(new File(nomArchivo)).delete();
+					(new File(nomArchivoNoti)).delete();
+				}
 				iterMediosPrensa++;
 			}
 		} catch (Exception e) {
